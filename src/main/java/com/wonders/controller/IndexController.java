39c30f1f824c4b39d4f72483c40458f8a874710b
wuzhiheng <335639905@qq.com>
2020-03-18 16:17:01 +0800

    个人中心

--- src/main/java/com/wonders/controller/IndexController.java
+++ src/main/java/com/wonders/controller/IndexController.java
@@ -27,59 +27,27 @@ public class IndexController extends BaseController{
         return "index";
     }
 
-    //综管指标
-    @RequestMapping("page/indicator")
-    public String indicator() {
-        request.getSession(true);
-        return "pages/indicator";
-    }
+//    //门户网站首页
+//    @RequestMapping("page/portal")
+//    public String portal(){
+//        return "pages/portal/index";
+//    }
+//    //门户网站套餐介绍
+//    @RequestMapping("page/package")
+//    public String packages(){
+//        return "pages/portal/package";
+//    }
+//
+//    //个人中心
+//    @RequestMapping("page/user")
+//    public String user(){
+//        return "pages/user/index";
+//    }
 
-    //慢病查询
-    @RequestMapping("page/ncd")
-    public String ncd() {
-        return "pages/ncd";
-    }
-
-    //综管指标说明
-    @RequestMapping("page/indicatorDesc")
-    public String indicatorDesc() {
-        return "pages/indicatorDesc";
-    }
-
-    //综管指标来源说明
-    @RequestMapping("page/indicatorDesc/{id}")
-    public String indicatorDesc_id(@PathVariable("id")String id, Model model) {
-        model.addAttribute("data",zgIndicatorDescService.getById(id));
-        return "pages/indicatorDesc_id";
-    }
-
-    //医疗机构树
-    @RequestMapping("page/orgTree")
-    public String orgTree() {
-        return "pages/orgTree";
-    }
-
-    //药品管理
-    @RequestMapping("page/drug")
-    public String drug() {
-        return "pages/drug";
-    }
-
-    //门户网站首页
-    @RequestMapping("page/portal")
-    public String portal(){
-        return "pages/portal/index";
-    }
-    //门户网站套餐介绍
-    @RequestMapping("page/package")
-    public String packages(){
-        return "pages/portal/package";
-    }
-
-    //个人中心
-    @RequestMapping("page/user")
-    public String user(){
-        return "pages/user/index";
+    //网页跳转-后期细分
+    @RequestMapping("page/**")
+    public String page(){
+        return "pages/"+request.getServletPath().replaceFirst("page/","");
     }
 
 
